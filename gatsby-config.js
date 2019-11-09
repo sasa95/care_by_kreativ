7136c40daac388414c3c62b4495f593fc4a4b838
Sasa Drmic <sasadrmic032@gmail.com>
2019-11-09 19:28:04 +0100

    Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    title: `Care by Kreativ`,
+    description: 'This is our digital agency.',
+  },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
---
+++ src/pages/components/head.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const Head = ({ title }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `)
+
+  return (
+    <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
+      <meta
+        name="description"
+        content={`${data.site.siteMetadata.description}`}
+      />
+    </Helmet>
+  )
+}
+export default Head
