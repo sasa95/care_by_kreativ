e10347fe659565ac884b7744bd3c7e57fefb6ed2
Sasa Drmic <sasadrmic032@gmail.com>
2020-01-05 15:54:09 +0100

    Fixed jurky scrollbar

--- src/components/bubbles/bubbles-body.js
+++ src/components/bubbles/bubbles-body.js
@@ -6,7 +6,7 @@ const randomColors = Object.values(colors)
 
 const bodyAnimation = keyframes`
   0% {
-    top: 100%;
+    top: 99%;
     opacity: 1
   } 
 
@@ -44,7 +44,8 @@ const getAnimations = number => {
 }
 
 const BodyBubbles = styled.div`
-  ${getAnimations(5)}
+  height: 15px;
+  ${getAnimations(5)};
 `
 
 const generateBubbles = number => {
@@ -52,7 +53,7 @@ const generateBubbles = number => {
 }
 
 const Bubble = styled.span`
-  display: block;
+  display: table-cell;
   width: 12px;
   height: 12px;
   border-radius: 50%;
