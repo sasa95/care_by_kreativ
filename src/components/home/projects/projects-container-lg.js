67f2c6c8103d58ab72a0abc99576ab3bfe3e4c4b
Sasa Drmic <sasadrmic032@gmail.com>
2020-02-20 11:50:36 +0100

    :dizzy: Use gsap for carousel indicators animations

--- src/components/home/projects/projects-container-lg.js
+++ src/components/home/projects/projects-container-lg.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import styled, { keyframes, css } from 'styled-components'
+import styled from 'styled-components'
 import ProjectsCarousel from './projects-carousel'
 import { Container } from '@styles/shared'
-
-const grow = keyframes`
-  to { width: 100% }
-`
+import gsap from 'gsap'
 
 const Projects = styled.section`
   ${Container}
@@ -37,27 +34,32 @@ const Indicator = styled.div`
   border-radius: 10px;
   background: rgba(51, 51, 51, 0.1);
   cursor: pointer;
+`
 
-  &:after {
-    ${({ i, a, color }) =>
-      i === a &&
-      css`
-        position: absolute;
-        display: block;
-        width: 0;
-        height: 5px;
-        border-radius: 10px;
-        background: ${color};
-        content: '';
-      `};
-    animation: 6s ${grow} 0.8s linear;
-  }
+const IndicatorProgress = styled.div`
+  background: ${({ color }) => color};
+  position: absolute;
+  width: 0;
+  height: 5px;
+  border-radius: 10px;
 `
 
 const ProjectsContainerLG = ({ projects }) => {
   const [selectedProjectIndex, setSelectedProjectIndex] = useState(0)
+  const progressRefs = []
+
+  const changeProject = index => {
+    setSelectedProjectIndex(index)
+  }
 
   useEffect(() => {
+    const tween = gsap.to(progressRefs[selectedProjectIndex], {
+      width: '100%',
+      ease: 'none',
+      duration: 6,
+      delay: 0.8,
+    })
+
     const timeout = setTimeout(() => {
       let newIndex
       if (selectedProjectIndex === projects.length - 1) {
@@ -68,13 +70,11 @@ const ProjectsContainerLG = ({ projects }) => {
       setSelectedProjectIndex(newIndex)
     }, 7000)
     return () => {
+      tween.seek(0)
+      tween.kill()
       clearTimeout(timeout)
     }
   }, [selectedProjectIndex])
-
-  const changeProject = index => {
-    setSelectedProjectIndex(index)
-  }
 
   return (
     <Projects id="projects">
@@ -82,13 +82,12 @@ const ProjectsContainerLG = ({ projects }) => {
 
       <IndicatorsContainer>
         {projects.map((project, i) => (
-          <Indicator
-            onClick={() => changeProject(i)}
-            key={i}
-            i={i}
-            a={selectedProjectIndex}
-            color={project.color}
-          />
+          <Indicator onClick={() => changeProject(i)} key={i}>
+            <IndicatorProgress
+              color={project.color}
+              ref={e => e && progressRefs.push(e)}
+            />
+          </Indicator>
         ))}
       </IndicatorsContainer>
     </Projects>
