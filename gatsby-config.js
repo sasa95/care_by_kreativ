972fcce1bcde65b3a724e0302271b9339276292d
Sasa Drmic <sasadrmic032@gmail.com>
2020-03-02 09:57:37 +0100

    :heavy_plus_sign: Add webpack-bundle-analyser dev dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-preact`,
+    `gatsby-plugin-webpack-bundle-analyser-v2`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
